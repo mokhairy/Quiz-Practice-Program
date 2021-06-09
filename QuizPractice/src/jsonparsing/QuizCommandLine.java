bc9d4c2d3cb4641ed9dc06c26c172ef5fe96a1a0
danwink01 <47534454+danwink01@users.noreply.github.com>
2021-06-09 13:31:45 -0400

    Update QuizCommandLine.java

--- QuizPractice/src/jsonparsing/QuizCommandLine.java
+++ QuizPractice/src/jsonparsing/QuizCommandLine.java
@@ -17,19 +17,22 @@ import pojo.QuestionPOJO;
 class QuizCommandLine {
 
 	public static void main(String[] args) throws IllegalArgumentException, IOException {
+		// Creation of objects needed to excecute the program
 		Scanner scnr = new Scanner(System.in);
 		System.out.println("Please paste the absolute path to your JSON Quiz!");
 		String filePath = scnr.nextLine();
 		File quizFile = new File(filePath);
 		JsonNode node = Json.parseFile(quizFile);
 		QuizPOJO pojo = Json.fromJson(node, QuizPOJO.class);
-		int userChoice = 0;
+		char userChoice;
 		
+		// Initial heading upon program start
 		System.out.println("Program: " + pojo.getProgramName());
 		System.out.println("Author: " + pojo.getAuthorName());
 		System.out.println("Date Published: " + pojo.getDatePublished());
 		System.out.println("---------------------------------------------------------------------------------------------------------------------------------------\n");
 		
+		// Enhanced for loop that loops through every question in the file specified
 		for(QuestionPOJO question:pojo.getQuestions()) {
 			System.out.println("Question: " + question.getQuestion());
 			if(question.getAnswer1() != null) {
@@ -45,6 +48,8 @@ class QuizCommandLine {
 				System.out.println("D: " + question.getAnswer4());
 			}
 			
+			// Get the user answer as a single character.
+			// Only a/A b/B c/C or d/D are valid
 			System.out.print("Enter your answer: ");
 			userChoice = scnr.next().charAt(0);
 			System.out.println();
